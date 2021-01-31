119e3eaf43d9ea3a333b30656e833b99a4104d1c
padamv <padamv91@gmail.com>
2021-01-31 11:26:01 +0100

    applying screening

--- src/com/learn/java/functionalInterfaces/ConsumerExample.java
+++ src/com/learn/java/functionalInterfaces/ConsumerExample.java
@@ -7,20 +7,32 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
+    static Consumer<Student> c2 = (student) -> System.out.println(student);
+    static Consumer<Student> c3 = (student) -> System.out.print(student.getName());
+    static Consumer<Student> c4 = (student) -> System.out.println(student.getActivities());
+
 
     public static void printName() {
-        Consumer<Student> c2 = (student) -> System.out.println(student);
         List<Student> studentList = StudentDataBase.getAllStudents();
         studentList.forEach(c2);
     }
 
     public static void printNameAndActivities() {
-        Consumer<Student> c3 = (student) -> System.out.print(student.getName());
-        Consumer<Student> c4 = (student) -> System.out.println(student.getActivities());
-
+        System.out.println("printNameAndActivities");
         List<Student> studentList = StudentDataBase.getAllStudents();
 
         studentList.forEach(c3.andThen(c4));  // consumer chain
+    }
+
+    public static void printNameAndActivitiesUsingCondition() {
+        System.out.println("printNameAndActivitiesUsingCondition");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach((student -> {
+            if(student.getGradeLevel() >= 3 && student.getGpa() >= 3.9){
+                c3.andThen(c4).accept(student);
+            }
+        }));
     }
 
     public static void main(String[] args) {
@@ -28,7 +40,8 @@ public class ConsumerExample {
         Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
 
         c1.accept("java8");
-        printName();
+        //printName();
         printNameAndActivities();
+        printNameAndActivitiesUsingCondition();
     }
 }
