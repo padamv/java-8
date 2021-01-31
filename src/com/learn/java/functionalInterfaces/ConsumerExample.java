52880c8b34bfcfe0bc522dd94d00d01acc194128
padamv <padamv91@gmail.com>
2021-01-31 11:12:30 +0100

    ForEach example

--- src/com/learn/java/functionalInterfaces/ConsumerExample.java
+++ src/com/learn/java/functionalInterfaces/ConsumerExample.java
@@ -1,12 +1,24 @@
 package com.learn.java.functionalInterfaces;
 
+import com.learn.java.data.Student;
+import com.learn.java.data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
+
+    public static void printName() {
+        Consumer<Student> c2 = (student) -> System.out.println(student);
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(c2);
+    }
+
     public static void main(String[] args) {
 
         Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
 
         c1.accept("java8");
+        printName();
     }
 }
