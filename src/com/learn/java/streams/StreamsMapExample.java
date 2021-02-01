0c64974463f89d3c51749797a95282a67b39d8d8
padamv <padamv91@gmail.com>
2021-02-01 15:23:38 +0100

    Add method to return set

--- src/com/learn/java/streams/StreamsMapExample.java
+++ src/com/learn/java/streams/StreamsMapExample.java
@@ -4,6 +4,7 @@ import com.learn.java.data.Student;
 import com.learn.java.data.StudentDataBase;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamsMapExample {
@@ -19,7 +20,19 @@ public class StreamsMapExample {
         return studentList;
     }
 
+    public static Set<String> namesSet(){
+
+        Set<String> studentSet = StudentDataBase.getAllStudents().stream()  // Stream<Student>
+                // Student as an input -> Student Name
+                .map(Student::getName)    // Stream<String>
+                .map(String::toUpperCase)  // Stream<String> -> uppercase operation on each input
+                .collect(Collectors.toSet());  //Set<String>
+
+        return studentSet;
+    }
+
     public static void main(String[] args) {
         System.out.println(namesList());
+        System.out.println(namesSet());
     }
 }
