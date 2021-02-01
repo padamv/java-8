680e920f7b2e1f870b84707ce14dec239d799f61
padamv <padamv91@gmail.com>
2021-02-01 15:33:59 +0100

    Use of flatMap

---
+++ src/com/learn/java/streams/StreamsFlatMapExample.java
@@ -0,0 +1,24 @@
+package com.learn.java.streams;
+
+import com.learn.java.data.Student;
+import com.learn.java.data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsFlatMapExample {
+
+    public static List<String> printStudentActivities() {
+
+        List<String> studentActivities = StudentDataBase.getAllStudents().stream()  //Stream<Student>
+                .map(Student::getActivities)  // Stream<List<String>>
+                .flatMap(List::stream) // Stream<String>
+                .collect(Collectors.toList());
+
+        return studentActivities;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("printStudentActivities: " + printStudentActivities());
+    }
+}
