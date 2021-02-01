bfbd9c9a0ee3a652420f7748a45ea283ae5ca438
padamv <padamv91@gmail.com>
2021-02-01 15:09:03 +0100

    Difference highlighted

---
+++ src/com/learn/java/streams/CollectionsVsStream.java
@@ -0,0 +1,30 @@
+package com.learn.java.streams;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class CollectionsVsStream {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("adam");
+        names.add("jim");
+        names.add("jenny");
+
+        names.remove(0);
+
+        System.out.println(names);
+
+        for(String name : names){
+            System.out.println(name);
+        }
+
+        for(String name : names){
+            System.out.println(name);
+        }
+
+        Stream<String> nameStream = names.stream();
+        nameStream.forEach(System.out::println);
+        // nameStream.forEach(System.out::println);  WILL THROW IllegalStateException
+
+    }
+}
