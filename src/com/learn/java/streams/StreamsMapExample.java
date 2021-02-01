fff70c9acce645534f9007bb20c936060cf93f25
padamv <padamv91@gmail.com>
2021-02-01 15:19:16 +0100

    Basic feature of map

---
+++ src/com/learn/java/streams/StreamsMapExample.java
@@ -0,0 +1,24 @@
+package com.learn.java.streams;
+
+import com.learn.java.data.Student;
+import com.learn.java.data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsMapExample {
+
+    public static List<String> namesList(){
+
+        List<String> studentList = StudentDataBase.getAllStudents().stream()  // Stream<Student>
+                // Student as an input -> Student Name
+                .map(Student::getName)    // Stream<String>
+                .collect(Collectors.toList());  //List<String>
+
+        return studentList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(namesList());
+    }
+}
