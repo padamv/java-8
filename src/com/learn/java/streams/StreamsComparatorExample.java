0a6dc822d514a1c8db6b2edc626cd73966af6737
padamv <padamv91@gmail.com>
2021-02-01 15:56:38 +0100

    Order by gpa

--- src/com/learn/java/streams/StreamsComparatorExample.java
+++ src/com/learn/java/streams/StreamsComparatorExample.java
@@ -17,8 +17,30 @@ public class StreamsComparatorExample {
                 .collect(Collectors.toList());
     }
 
+    public static List<Student> sortStudentsByGPA(){
+
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .sorted(Comparator.comparing(Student::getGpa))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Student> sortStudentsByGPADescending(){
+
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .sorted(Comparator.comparing(Student::getGpa).reversed())
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         System.out.println("Students sorted by Name: ");
         sortStudentsByName().forEach(System.out::println);
+
+        System.out.println("Students sorted by GPA: ");
+        sortStudentsByGPA().forEach(System.out::println);
+
+        System.out.println("Students sorted by GPA DESC: ");
+        sortStudentsByGPADescending().forEach(System.out::println);
     }
 }
