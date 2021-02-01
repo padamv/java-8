4c1412b02af9ca67d371462afdcd68a6b2bb49c5
padamv <padamv91@gmail.com>
2021-02-01 16:36:50 +0100

    reduce method without the identity value

--- src/com/learn/java/streams/StreamReduceExample.java
+++ src/com/learn/java/streams/StreamReduceExample.java
@@ -3,6 +3,7 @@ package com.learn.java.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamReduceExample {
 
@@ -20,11 +21,27 @@ public class StreamReduceExample {
                 .reduce(1, (a, b) -> a * b);
     }
 
+    public static Optional<Integer> performMultiplicationWithoutIdentity(List<Integer> integerList) {
+
+        return integerList.stream()
+                .reduce( (a, b) -> a * b);
+    }
+
     public static void main(String[] args) {
 
         List<Integer> integers = Arrays.asList(1, 3, 5, 7);
+        List<Integer> integers1 = new ArrayList<>();
 
         System.out.println(performMultiplication(integers));
+
+        Optional <Integer> result = performMultiplicationWithoutIdentity(integers);
+
+        System.out.println(result.isPresent());
+        System.out.println(result.get());
+
+        Optional <Integer> result1 = performMultiplicationWithoutIdentity(integers1);
+
+        System.out.println(result1.isPresent());
 
     }
 }
