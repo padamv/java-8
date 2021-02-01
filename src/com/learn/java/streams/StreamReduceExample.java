941bdf3eba1a3e18c60a392a10fc473febc549f7
padamv <padamv91@gmail.com>
2021-02-01 16:42:10 +0100

    Get student with highest gpa with reduce method

--- src/com/learn/java/streams/StreamReduceExample.java
+++ src/com/learn/java/streams/StreamReduceExample.java
@@ -1,5 +1,8 @@
 package com.learn.java.streams;
 
+import com.learn.java.data.Student;
+import com.learn.java.data.StudentDataBase;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,6 +30,17 @@ public class StreamReduceExample {
                 .reduce( (a, b) -> a * b);
     }
 
+    public static Optional<Student> getHighestGPAStudent(){
+        return StudentDataBase.getAllStudents().stream()
+                .reduce((s1,s2) -> {
+                        if(s1.getGpa() > s2.getGpa()){
+                            return s1;
+                        }else{
+                            return s2;
+                        }
+                });
+    }
+
     public static void main(String[] args) {
 
         List<Integer> integers = Arrays.asList(1, 3, 5, 7);
@@ -42,6 +56,11 @@ public class StreamReduceExample {
         Optional <Integer> result1 = performMultiplicationWithoutIdentity(integers1);
 
         System.out.println(result1.isPresent());
+
+        Optional<Student> studentOptional = getHighestGPAStudent();
+        if(getHighestGPAStudent().isPresent()){
+            System.out.println(studentOptional.get());
+        }
 
     }
 }
