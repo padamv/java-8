0aff463cd8c35184f8514561f2b704623db0ff86
padamv <padamv91@gmail.com>
2021-02-01 17:00:18 +0100

    Refactor reduce

--- src/com/learn/java/streams/StreamMapReduceExample.java
+++ src/com/learn/java/streams/StreamMapReduceExample.java
@@ -8,7 +8,8 @@ public class StreamMapReduceExample {
     private static int noOfNoteBooks(){
         Integer noOfNoteBooks = StudentDataBase.getAllStudents().stream() // Stream<Student>
                 .map(Student::getNoteBooks)  //Stream<Integer>
-                .reduce(0, (a, b) -> a + b);
+                //.reduce(0, (a, b) -> a + b);
+                .reduce(0, Integer::sum);
 
         return noOfNoteBooks;
     }
