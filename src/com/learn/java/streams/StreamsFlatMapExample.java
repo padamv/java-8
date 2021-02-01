51a01580c290b1f1d707931e874d3a01231008a9
padamv <padamv91@gmail.com>
2021-02-01 15:42:18 +0100

    sorted and count functions

--- src/com/learn/java/streams/StreamsFlatMapExample.java
+++ src/com/learn/java/streams/StreamsFlatMapExample.java
@@ -13,13 +13,26 @@ public class StreamsFlatMapExample {
         List<String> studentActivities = StudentDataBase.getAllStudents().stream()  //Stream<Student>
                 .map(Student::getActivities)  // Stream<List<String>>
                 .flatMap(List::stream) // Stream<String>
-                .distinct()
+                .distinct()   // Stream<String> -> with distinct values
+                .sorted()
                 .collect(Collectors.toList());
 
         return studentActivities;
     }
 
+    public static Long getStudentActivitiesCount() {
+
+        Long numberOfStudentActivities = StudentDataBase.getAllStudents().stream()  //Stream<Student>
+                .map(Student::getActivities)  // Stream<List<String>>
+                .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> -> with distinct values
+                .count();
+
+        return numberOfStudentActivities;
+    }
+
     public static void main(String[] args) {
         System.out.println("printStudentActivities: " + printStudentActivities());
+        System.out.println("Number of student activities: " + getStudentActivitiesCount());
     }
 }
