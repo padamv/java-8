2d05edbbe181956f7dd6bc4e138f65dcfa3b97d1
padamv <padamv91@gmail.com>
2021-02-02 11:57:45 +0100

    Boxing and unboxing streams

---
+++ src/com/learn/java/numericstreams/NumericStreamsBoxingUnBoxingExample.java
@@ -0,0 +1,31 @@
+package com.learn.java.numericstreams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamsBoxingUnBoxingExample {
+
+    public static List<Integer> boxing() {
+
+        return IntStream.rangeClosed(1, 10)
+                //int
+                .boxed()
+                //Integer
+                .collect(Collectors.toList());
+    }
+
+    public static int unBoxing(List<Integer> integerList){
+        //wrapper to primitive
+        return integerList.stream()
+                .mapToInt(Integer::intValue) //intStream (intValue of the Wrapper class)
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Boxing: " + boxing());
+
+        List<Integer> integerList = boxing();
+        System.out.println("Unboxing: " + unBoxing(integerList));
+    }
+}
