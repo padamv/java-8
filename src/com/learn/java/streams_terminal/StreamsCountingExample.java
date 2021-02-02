f7559e553ecab7f42dd3154ab1d7833096ef36da
padamv <padamv91@gmail.com>
2021-02-02 12:26:23 +0100

    Application of Collectors.counting

---
+++ src/com/learn/java/streams_terminal/StreamsCountingExample.java
@@ -0,0 +1,20 @@
+package com.learn.java.streams_terminal;
+
+import com.learn.java.data.StudentDataBase;
+
+import static java.util.stream.Collectors.counting;
+
+public class StreamsCountingExample {
+
+    public static long count(){
+        return StudentDataBase.getAllStudents().stream()
+                .filter(student -> student.getGpa() >= 3.9)
+                .collect(counting());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(count());
+
+    }
+}
