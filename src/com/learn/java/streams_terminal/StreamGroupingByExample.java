dfaa370aa40243ff85d61643b419b6d137b8fd57
padamv <padamv91@gmail.com>
2021-02-02 13:27:21 +0100

    Two parameter groupingBy

--- src/com/learn/java/streams_terminal/StreamGroupingByExample.java
+++ src/com/learn/java/streams_terminal/StreamGroupingByExample.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class StreamGroupingByExample {
 
@@ -26,8 +27,27 @@ public class StreamGroupingByExample {
         System.out.println(studentMap);
     }
 
+    public static void twoLevelGrouping_1(){
+
+        Map<Integer, Map<String, List<Student>>> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel,
+                        groupingBy(student -> student.getGpa() >= 3.8 ? "OUTSTANDING" : "AVERAGE")));
+        System.out.println(studentMap);
+    }
+
+    public static void twoLevelGrouping_2(){
+
+        Map<Integer, Integer> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel,
+                        summingInt(Student::getNoteBooks)));
+        System.out.println(studentMap);
+    }
+
     public static void main(String[] args) {
         //groupStudentsByGender();
-        customizeGroupingBy();
+        //customizeGroupingBy();
+        twoLevelGrouping_2();
     }
 }
