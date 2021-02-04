5db82fe530db90c1d9642b7aef8d911c85f7e128
padamv <padamv91@gmail.com>
2021-02-04 09:34:21 +0100

    Difference between interface default and static methods presented

---
+++ src/com/learn/java/defaults/MultiplierClient.java
@@ -0,0 +1,16 @@
+package com.learn.java.defaults;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiplierClient {
+
+    public static void main(String[] args) {
+        Multiplier multiplier = new MultiplierImpl();
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
+
+        System.out.println("Result is: " + multiplier.multiply(integerList));
+        System.out.println("default method size is: " + multiplier.size(integerList));
+        System.out.println("static method isEmpty: " + Multiplier.isEmpty(integerList));
+    }
+}
