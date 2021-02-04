3456944d25133b321ead273c8c5a82dda5e10112
padamv <padamv91@gmail.com>
2021-02-04 09:57:05 +0100

    Introduce LocalDate, LocalTime & LocalDateTime

---
+++ src/com/learn/java/dates/NewDateTimeExample.java
@@ -0,0 +1,25 @@
+package com.learn.java.dates;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class NewDateTimeExample {
+
+
+    public static void main(String[] args) {
+
+        //LocalDate
+        LocalDate localDate = LocalDate.now();
+        System.out.println("localDate: " + localDate);
+
+        //LocalTime
+        LocalTime localTime = LocalTime.now();
+        System.out.println("localTime: " + localTime);
+
+        //LocalDateTime
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("localDateTime: " + localDateTime);
+
+    }
+}
